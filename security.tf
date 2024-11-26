de231c7869f5773c3cf87c813fa82b6f901fae66
Gitpractice <sarath181410@gmail.com>
2024-11-26 12:18:46 +0530

    dfaf

--- security.tf
+++ security.tf
@@ -5,27 +5,27 @@ resource "aws_security_group" "my-sg" {
   ingress {
     from_port = "22"
     to_port = "22"
-    protocal = "tcp"
+    protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress{
     from_port = 80
     to_port = 80
-    protocal = "tcp"
+    protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
 
   }
   egress{
     from_port = 443
     to_port = 443
-    protocal = "tcp"
+    protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
 
   }
   egress{
     from_port = 0
     to_port = 0
-    protocal = "tcp"
+    protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
 
   }
